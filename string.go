56d03bf60f687f9cc3353b342c405c11a9083bab
Whena <40749131+Whena@users.noreply.github.com>
2021-10-12 23:39:52 +0700

    string_update

--- string.go
+++ string.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("whena")
 	fmt.Println("whena wija")
 	fmt.Println("whena wijanarko")
+
+	fmt.Println(len("whena"))
+	fmt.Println("whena wija"[0])
+	fmt.Println("whena wijanarko"[1])
 }
